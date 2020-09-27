352d5ad26a03a67c7a17e1577cdbd5b16d2cb761
Montilla Francisco <montilla2808@gmail.com>
2020-09-27 16:42:30 +0200

    Initial commit

---
+++ src/test/java/fmont/sds/SdsApplicationTests.java
@@ -0,0 +1,13 @@
+package fmont.sds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
